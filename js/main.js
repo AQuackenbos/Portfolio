4246a597af3002100c51de198599aba5594a60e1
Andrew Quackenbos <andrew@sitesquad.net>
2013-02-16 18:02:14 -0500

    Fix to main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,22 @@
 (function(){
-window.Portfolio = {
-	Models: {},
-	Collections: {},
-	Views: {},
-	Router: {}
-};
+	window.Portfolio = {
+		Models: {},
+		Collections: {},
+		Views: {},
+		Router: {}
+	};
 
-window.template = function(templateId) {
-	return _.template($('#'+templateId).html());
-};
+	window.template = function(templateId) {
+		return _.template($('#'+templateId).html());
+	};
 
-Portfolio.Models.Page = Backbone.Model.extend({
-	urlRoot: '/page'
-});
-Portfolio.Collections.Pages = Backbone.Collec
+	Portfolio.Models.Page = Backbone.Model.extend({
+		urlRoot: '/page'
+	});
+
+	Portfolio.Collections.Pages = Backbone.Collection.extends({
+		model: Portfolio.Models.Page,
+		url: '',
+	});
+
 })();
