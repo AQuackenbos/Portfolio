ba1de187bbf0cfc257196b79136de203dc8d272f
Andrew Quackenbos <andrew@sitesquad.net>
2013-02-21 15:59:07 -0500

    Tests... that work this time

--- js/main.js
+++ js/main.js
@@ -1,22 +1,46 @@
 var Portfolio = Ember.Application.create();
 
-Portfolio.ApplicationView = Ember.View.extend({
-	templateName: 'application'
+Portfolio.ApplicationController = Ember.Controller.extend({
+	test: "Test!"
 });
-Portfolio.ApplicationController = Ember.Controller.extend();
 
 Portfolio.Router.reopen({
 	location: 'history'
 });
 
 
-Portfolio.IndexView = Ember.View.extend({
-	templateName: 'contributors'
-});
-Portfolio.IndexController = Ember.Controller.extend({
-	test: 'test!'
+Portfolio.IndexRoute = Ember.Route.extend({
+	setupController: function(controller){
+		controller.set('title',"Better title...");
+	}
 });
 
 Portfolio.Router.map(function(){
+	this.route('contributors');
+	this.route('contributor', {path: '/contributors/:contributor_id'});
+});
 
+Portfolio.Contributor = Ember.Object.extend();
+Portfolio.Contributor.reopenClass({
+  allContributors: [],
+  all: function(){
+    this.allContributors = [];
+    $.ajax({
+      url: '/js/test.json',
+      dataType: 'json',
+      context: this,
+      success: function(response){
+        response.forEach(function(contributor){
+          this.allContributors.addObject(Portfolio.Contributor.create(contributor))
+        }, this)
+      }
+    })
+    return this.allContributors;
+  }
+});
+
+Portfolio.ContributorsRoute = Ember.Route.extend({
+	model: function() {
+		return Portfolio.Contributor.all();
+	}
 });
